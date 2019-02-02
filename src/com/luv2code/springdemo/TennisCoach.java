3383db6570a75100cd0e1de84716d2b103c80e60
imgopz <rrgkmech@gmail.com>
2019-02-02 14:59:57 +0530

    Setter Injection Example

--- src/com/luv2code/springdemo/TennisCoach.java
+++ src/com/luv2code/springdemo/TennisCoach.java
@@ -9,14 +9,31 @@ public class TennisCoach implements Coach {
 
 	private FortuneService fortuneService;
 	
+	// define default constructor to test
+	public TennisCoach() {
+		
+		System.out.println(">> TennisCoach: Inside default constructor");
+		
+	}
+	
+	
+	/*
 	@Autowired 
-	/* Constructor Injection - There is only one fortune service which implements the interface
-	FortuneServce i.e. HappyFortuneService. So this Autowire annotation just assign it to theForutnueService
-	and we can call te function insed getDailyFortune as fortuneService.getFortune() as below */ 
+//  Constructor Injection - There is only one fortune service which implements the interface FortuneServce i.e. HappyFortuneService. So this Autowire annotation just assign it to theForutnueService and we can call te function insed getDailyFortune as fortuneService.getFortune() as below 
 	public TennisCoach(FortuneService theFortuneService) {
 		
 		fortuneService = theFortuneService;
 				
+	} 
+	*/
+	
+	// define Setter mothod for setter Injection
+	@Autowired
+	public void setFortuneService(FortuneService theFortuneService) {
+		
+		System.out.println(">> TennisCoach: Inside setFortuneService() method");
+		fortuneService = theFortuneService;
+		
 	}
 	
 	@Override
