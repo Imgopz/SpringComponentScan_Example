5b46cdaa963129ca4752d757fbfbb56dd30a2a2f
imgopz <rrgkmech@gmail.com>
2019-02-02 16:50:00 +0530

    PostConstruct and PreDestroy annotations in sigleton

--- src/com/luv2code/springdemo/TennisCoach.java
+++ src/com/luv2code/springdemo/TennisCoach.java
@@ -1,13 +1,16 @@
 package com.luv2code.springdemo;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Scope;
+//import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 //@Component("thatSillyCoach") // Spring will automatically register this bean (i.e: Object)
 @Component // The default bean id will be tennisCoach
-@Scope("prototype")
+//@Scope("prototype")
 public class TennisCoach implements Coach {
 	
 	/*
@@ -34,7 +37,21 @@ public class TennisCoach implements Coach {
         fortuneService = theFortuneService;
     }
 	
+	// define init method
+	@PostConstruct
+	public void doMyStartupStuff() {
+		
+		System.out.println(">> TennisCoach: inside my doMyStartupStuff() method");
+		
+	}
 	
+	// define my destry method
+	@PreDestroy
+	public void doMyCleanupStuff() {
+		
+		System.out.println(">> TennisCoach: inside my doCleanupStuff() method");
+		
+	}
 	/*
 	@Autowired 
   	// Constructor Injection - There is only one fortune service which implements the interface FortuneServce i.e. HappyFortuneService. So this Autowire annotation just assign it to theForutnueService and we can call te function insed getDailyFortune as fortuneService.getFortune() as below 
