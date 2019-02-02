de7945fec059f339a9abf03f56225dfa4c3246e4
imgopz <rrgkmech@gmail.com>
2019-02-02 17:20:26 +0530

    defining spring beans in config.java file and using it

---
+++ src/com/luv2code/springdemo/GoodFortuneService.java
@@ -0,0 +1,12 @@
+package com.luv2code.springdemo;
+
+public class GoodFortuneService implements FortuneService {
+
+	@Override
+	public String getFortune() {
+		
+		return "Today is your good Day";
+		
+	}
+
+}
--- src/com/luv2code/springdemo/SportConfig.java
+++ src/com/luv2code/springdemo/SportConfig.java
@@ -1,10 +1,27 @@
 package com.luv2code.springdemo;
 
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("com.luv2code.springdemo")
+//@ComponentScan("com.luv2code.springdemo")
 public class SportConfig {
-
+	
+	// define bean for our good fortune service
+	@Bean
+	public FortuneService goodFortuneService() { // Bean id here is goodFortuneService
+		
+		return new GoodFortuneService();
+		
+	}
+	
+	// define bean for our swimCoach and inject dependency
+	@Bean
+	public Coach swimCoach() {
+		
+		return new SwimCoach(goodFortuneService()); // calling bean method from above method
+		
+	}
+		
 }
